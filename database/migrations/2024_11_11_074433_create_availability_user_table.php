5a85e1e4a7527416c3210b0183b84b262c29edd5
FathimaShums <fathimashums416@gmail.com>
2024-11-12 13:55:29 +0530

    database updates

--- database/migrations/2024_11_11_074433_create_availability_user_table.php
+++ database/migrations/2024_11_11_074433_create_availability_user_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Skills', function (Blueprint $table) {
+        Schema::create('availability_user', function (Blueprint $table) {
             $table->id();
-            $table->string('skill_name'); // Add a column for the skill name
-        $table->unsignedBigInteger('category_id'); // Foreign key column
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('availability_id')->constrained()->onDelete('cascade');
             $table->timestamps();
-
-            $table->foreign('category_id')->references('id')->on('Category')->onDelete('cascade');
         });
     }
 
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Skills');
+        Schema::dropIfExists('availability_user');
     }
 };
